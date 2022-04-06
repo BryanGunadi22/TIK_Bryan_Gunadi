3c0258f4f164365b7a1582b36f4a09daaa19058a
BryanGunadi22 <103086165+BryanGunadi22@users.noreply.github.com>
2022-04-06 11:16:08 +0800

    Create 20220406_variable.cpp

---
+++ 20220406_variable.cpp
@@ -0,0 +1,23 @@
+//header
+#include <iostream>
+
+int main () {//begin of function
+using namespace std;
+
+    cout<<"Bryan Kevyn Davvo Gunadi";
+    
+    string nama; //deklarasi variabel
+    nama="Bryan Kevyn Davvo Gunadi "; //masukkan value ke dalam variabel
+    
+    float angka,botak,Jecelyn;
+    
+    angka = 10.23;
+    botak = 3.14;
+    Jecelyn = 13;
+
+    
+    cout<<nama<<endl; //menampilkan variabel
+    cout<<angka+botak<<endl;
+    cout<<Jecelyn;
+    
+}//end of function
